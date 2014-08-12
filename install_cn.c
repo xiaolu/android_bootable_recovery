dda0f08842ea401d3242fa1dae2b8b3541fd3947
xiaolu <server@163.com>
2014-08-12 13:07:16 +0800

    Auto umount /system before install update package.
    Change-Id: I0a35830713d8e253a69282dadbcd60eecf282918

--- install_cn.c
+++ install_cn.c
@@ -379,6 +379,7 @@ really_install_package(const char *path)
     ui_set_background(BACKGROUND_ICON_INSTALLING);
     ui_print("找到刷机包...\n");
     ui_show_indeterminate_progress();
+    ensure_path_unmounted("/system");
 
     // Resolve symlink in case legacy /sdcard path is used
     // Requires: symlink uses absolute path
